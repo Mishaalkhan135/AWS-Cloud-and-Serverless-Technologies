a5543bb637053fa76f92c5787b91e7a03e336f88
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-03-09 21:51:19 +0500

    sns

---
+++ stpe16_Simple_Notification_Service/test/stpe16_simple_notification_service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Stpe16SimpleNotificationService from '../lib/stpe16_simple_notification_service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/stpe16_simple_notification_service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Stpe16SimpleNotificationService.Stpe16SimpleNotificationServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
